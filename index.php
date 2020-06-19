8db44e5b778ebd4782e39c98ee41effa3f01ee5e
Anurag Kumar Mishra <59967041+anuragmishra021@users.noreply.github.com>
2020-06-19 22:26:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <html>
 <body>
-<h1>Anurag Muishra</h1>
-<h3>MY FIRST AWS CLOUD TASK </h3>
+<h1>Anurag Mishra</h1>
+<h1>MY FIRST AWS CLOUD TASK </h1>
+<p>
 <?php
    $cloudant_url=`head -n1 path.txt`;
    $img_path="http://".$cloudant_url."/terraform.png";
    echo "<br>";
    echo "<img src='${img_path}' width=750 height=422>";
 ?>
+</p>
 <h3>Terraform AWS : Terraform by HashiCorp, an AWS Partner Network (APN) Advanced Technology Partner and member of the AWS 
    DevOps Competency, is an “infrastructure as code” tool similar to AWS CloudFormation that allows you to create, update, and version your Amazon Web Services (AWS) infrastructure.</h3>
+   <h3> In order to know more in detain about task please go through my article : </h3>
+   <h3> Complete Code At :- https://github.com/anuragmishra021/hybridcloudtask1 </h3>
+ Github Repo:-
 </body> 
 </html> 
