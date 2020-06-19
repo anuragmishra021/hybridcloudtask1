124a6a8cee6ea19567dbe3c4e8357fb756ce840b
Anurag Kumar Mishra <59967041+anuragmishra021@users.noreply.github.com>
2020-06-19 14:40:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 <body>
-<h1>KANIKA SHARMA</h1>
+<h1>Anurag Muishra</h1>
 <h3>MY FIRST AWS CLOUD TASK </h3>
 <?php
    $cloudant_url=`head -n1 path.txt`;
-   $img_path="https://".$cloudant_url."/terraform.png";
+   $img_path="http://".$cloudant_url."/terraform.png";
    echo "<br>";
    echo "<img src='${img_path}' width=100 height=100>";
 ?>
