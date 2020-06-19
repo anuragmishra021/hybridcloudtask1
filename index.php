15a87685b392112133c04a841eea8f150bfd0b18
Anurag Kumar Mishra <59967041+anuragmishra021@users.noreply.github.com>
2020-06-19 21:44:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
    $cloudant_url=`head -n1 path.txt`;
    $img_path="http://".$cloudant_url."/terraform.png";
    echo "<br>";
-   echo "<img src='${img_path}' width=100 height=100>";
+   echo "<img src='${img_path}' width=750 height=422>";
 ?>
+<h3>Terraform AWS : Terraform by HashiCorp, an AWS Partner Network (APN) Advanced Technology Partner and member of the AWS 
+   DevOps Competency, is an “infrastructure as code” tool similar to AWS CloudFormation that allows you to create, update, and version your Amazon Web Services (AWS) infrastructure.</h3>
 </body> 
 </html> 
