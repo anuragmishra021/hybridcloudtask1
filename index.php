1c39c198060dda92bbbad3d3fa424ed13d72bed9
Anurag Kumar Mishra <59967041+anuragmishra021@users.noreply.github.com>
2020-06-19 22:52:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </p>
 <h3>Terraform AWS : Terraform by HashiCorp, an AWS Partner Network (APN) Advanced Technology Partner and member of the AWS 
    DevOps Competency, is an “infrastructure as code” tool similar to AWS CloudFormation that allows you to create, update, and version your Amazon Web Services (AWS) infrastructure.</h3>
-   <h3> In order to know more in detain about task please go through my article : https://www.linkedin.com/pulse/infrastructure-code-terraform-automate-aws-cloud-anurag-kumar-mishra/ </h3>
+   <h3> In order to know more in detail about task please go through my article : https://www.linkedin.com/pulse/infrastructure-code-terraform-automate-aws-cloud-anurag-kumar-mishra/ </h3>
    <h3> Complete Code At :- https://github.com/anuragmishra021/hybridcloudtask1  </h3>
 </body> 
 </html> 
